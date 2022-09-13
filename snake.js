d55a00021f9971a72ff5938b5516e647fac01820
Mbaal <1046327@mborijnland.nl>
2022-09-13 13:07:27 +0200

    Snake vierkantje getekend

---
+++ snake.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById('snake');
+let context = canvas.getContext('2d');
+
+
+context.fillStyle = "black";
+context.fillRect(0, 0, 400, 400);
+
+context.fillStyle = "white";
+context.fillRect(200, 200, 20, 20);
